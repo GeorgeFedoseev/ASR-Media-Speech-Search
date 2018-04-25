839a08832e06f17d281e3f53e7175f0ad4a27bce
George Fedoseev <george.fedoseev@me.com>
2018-04-25 15:50:14 +0300

    freeze reuirements

--- const.py
+++ const.py
@@ -9,6 +9,10 @@ VIDEO_DATA_DIR = os.path.join(DATA_DIR, "media_data/")
 
 DEEP_SPEECH_MODEL_PATH = os.path.join(DATA_DIR, "deepspeech_model/")
 
+TRANSCRIBED_DATA_PATH  = os.path.join(DATA_DIR, "transcribed_data/")
+TRANSCRIBED_SPEECH_SQLITE_DB_PATH = os.path.join(TRANSCRIBED_DATA_PATH, "transcribed_speech.sqlite3")
+
+
 
 # audio related
 SAMPLE_RATE = 16000
